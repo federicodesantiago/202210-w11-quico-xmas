221fcd9c51cc1c7bbb49820c7a0c32e56f1deb3c
Quico <federicodesantiago@gmail.com>
2022-12-31 10:42:12 +0100

    Empty commit

--- src/core/components/footer/footer.tsx
+++ src/core/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import './footer.css';
 export function Footer() {
     return (
         <footer aria-label="footer">
-            <address>Quico Christmas Challenge</address>
+            <address>Quico Christmas Challenge. </address>
             <p className="footerP">{new Date().toLocaleDateString()}</p>
         </footer>
     );
